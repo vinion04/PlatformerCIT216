a7c23e65113099d0ee1f5d75c5bb7ca5b4f96ebc
Vin Lettich <hgl6@pct.edu>
2025-02-03 15:15:32 -0500

    Fixed Movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,39 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-
+    //private
+    private Animator animator;
+    private SpriteRenderer sprite_r;
+    private Rigidbody2D body;
     private Vector2 movementVector;
+    private bool isGrounded = false;
+    private bool jump = false;
+
+    //public
     public float speed = 3;
     public float jumpForce = 300;
-
-    public Animator animator;
-    public SpriteRenderer sprite_r;
+    public float maxSpeed = 7f;
+    public float gravityMultiplier = 3f;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         sprite_r = GetComponent<SpriteRenderer>();
+        body = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         animator.SetFloat("speed", Mathf.Abs(movementVector.x));
-        if (movementVector.x > 0)
+        if (movementVector.x > 0 && Mathf.Abs( body.velocity.x) < maxSpeed)
         {
             sprite_r.flipX = false;
-            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            body.AddForce(Vector2.right * speed);
         }
 
-        else if (movementVector.x < 0)
+        else if (movementVector.x < 0 && Mathf.Abs(body.velocity.x) < maxSpeed)
         {
             sprite_r.flipX = true;
-            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            body.AddForce(Vector2.left * speed);
         }
 
+        if(jump)
+        {
+            //StartCoroutine("LerpJump");
+            body.AddForce(Vector2.up * jumpForce);
+            jump = false;
+            isGrounded = false;
+        }
+
+        if(body.velocity.y < 0) //falling
+        {
+            body.gravityScale = gravityMultiplier;
+        }
+        else
+        {
+            body.gravityScale = 1;
+        }
 
     }
 
@@ -46,7 +71,26 @@ public class PlayerController : MonoBehaviour
 
     public void OnJump(InputValue movementValue)
     {
-        transform.Translate(Vector2.up * jumpForce * Time.deltaTime);
-        Debug.Log("Jumping");
+       if(isGrounded && jump == false)
+        {
+            jump = true;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            Debug.Log("Touching ground");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Boundary"))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
